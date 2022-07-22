b041d84207d20f2fb601861f6ad94ee6fe3a1df5
Edsson <akairu@hotmail.com>
2022-07-22 16:46:58 -0500

    sending fucntions as parameters

--- lib/reusable_cart.dart
+++ lib/reusable_cart.dart
@@ -3,19 +3,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
+  ReusableCard({required this.colour, this.cardChild, this.onPress});
   final Color colour;
   final Widget? cardChild; // not required allow nulls
-  ReusableCard({required this.colour, this.cardChild});
+  final VoidCallback? onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: cardChild,
       ),
-      child: cardChild,
     );
   }
 }
